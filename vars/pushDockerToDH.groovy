d21cb96c5fbb042c4920340f4ecc46d964af6094
PisethMao <pisethmao2002@gmail.com>
2026-01-13 09:07:06 +0000

    updated docker file again one more okay

--- vars/pushDockerToDH.groovy
+++ vars/pushDockerToDH.groovy
@@ -1,8 +1,11 @@
 def call(String imageName) {
+    def dockerfileContent = libraryResource('docker/Dockerfile')
+    writeFile file: 'Dockerfile', text: dockerfileContent
     withCredentials([usernamePassword(credentialsId: 'DOCKERHUB', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         // some block
         sh """
             set -e
+            docker build -t ${imageName} .
             echo "\$PASSWORD" | docker login -u "\$USERNAME" --password-stdin
             docker push ${imageName}
         """
