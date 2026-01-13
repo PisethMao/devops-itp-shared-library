f80fc8bf78c6841ea8cf0b447b5f723e818d0104
PisethMao <pisethmao2002@gmail.com>
2026-01-13 15:15:35 +0000

    aaaaa

--- vars/pushDockerToDH.groovy
+++ vars/pushDockerToDH.groovy
@@ -1,11 +1,8 @@
 def call(String imageName) {
-    def dockerfileContent = libraryResource('nextjs/Dockerfile')
-    writeFile file: 'Dockerfile', text: dockerfileContent
     withCredentials([usernamePassword(credentialsId: 'DOCKERHUB', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
         // some block
         sh """
             set -e
-            docker build -t ${imageName} .
             echo "\$PASSWORD" | docker login -u "\$USERNAME" --password-stdin
             docker push ${imageName}
         """
